5688c06cb1478b1457a2d05cb655ceb6e97194ad
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-21 20:08:47 +0530

    Update contact form submission route to save messages to the database .

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,9 +1,17 @@
+const ContactMessage = require("../models/ContactMessage");
+
 const submitContactForm = async (req, res) => {
   const { name, email, message } = req.body;
-  console.log(name, email, message);
-  res.status(200).json({
-    message: "We have received your message and we will get back to you soon.",
-  });
+  try {
+    const contactMessage = new ContactMessage({ name, email, message });
+    await contactMessage.save();
+    res.status(201).json({
+      message:
+        "We have receieved your message and we will get back to you soon.",
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = { submitContactForm };
