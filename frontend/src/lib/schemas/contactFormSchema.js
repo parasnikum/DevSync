4195fcc78a5ae6bacbf29e7e025a49826b06b6b9
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-21 21:07:34 +0530

    Update contact form zod schema to match validation of its mongoose schema

--- frontend/src/lib/schemas/contactFormSchema.js
+++ frontend/src/lib/schemas/contactFormSchema.js
@@ -5,7 +5,7 @@ const contactFormSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
   message: z
     .string()
-    .min(1, { message: "Message is required." })
+    .min(10, { message: "Message must be atleast 10 characters" })
     .max(500, { message: "Message cannot exceed 500 characters." }),
 });
 
