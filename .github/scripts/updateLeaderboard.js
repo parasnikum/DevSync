b57b45f1972da91dd9d11acd1b978b9d0eee7c1c
Annanya Tiwary <annanyatiwary4@gmail.com>
2025-09-12 21:44:51 +0530

    Update updateLeaderboard.js

--- .github/scripts/updateLeaderboard.js
+++ .github/scripts/updateLeaderboard.js
@@ -78,12 +78,9 @@ async function fetchAllPRsAndIssues() {
 }
 
 async function updateGoogleSheet(contributors) {
-  const auth = new google.auth.JWT(
-    process.env.GOOGLE_CLIENT_EMAIL,
-    null,
-    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    ["https://www.googleapis.com/auth/spreadsheets"]
-  );
+  const auth = new google.auth.GoogleAuth({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
 
   const sheets = google.sheets({ version: "v4", auth });
 
